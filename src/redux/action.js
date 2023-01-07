66294943e4b9894772101fb9a39abba5cca199da
abehera01 <abehera920@gmail.com>
2023-01-07 12:37:03 +0530

    add reducer and rootReducer file

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,7 +1,10 @@
+import { ADD_TO_CART } from "./constant";
+
 export const addToCart = (data) => {
     console.log("action called",data);
     return{
-        type: 'Add_TO_CART',
+        // type: 'Add_TO_CART',
+        type: ADD_TO_CART,
         // data: data ,
         data
     }
